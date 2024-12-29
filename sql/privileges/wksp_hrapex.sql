ef5a4deafdb0961cc381faffe824339cd8ca15a3
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-12-30 00:21:50 +0100

    Update app

--- sql/privileges/wksp_hrapex.sql
+++ sql/privileges/wksp_hrapex.sql
@@ -1,3 +1,5 @@
+grant all on hr.employees   to wksp_hrapex; 
+grant all on hr.departments to wksp_hrapex; 
+
 grant select on hr.v_employees to wksp_hrapex;
 
-grant all on hr.employees to wksp_hrapex; 
