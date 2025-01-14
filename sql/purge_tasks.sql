b5df8fa4e932211effdf39ddcb02511324b59d7d
Angelo Stramieri <angelo.stramieri@gmail.com>
2025-01-14 16:40:19 +0100

    Update app

--- sql/purge_tasks.sql
+++ sql/purge_tasks.sql
@@ -1,4 +1,9 @@
 begin
-  apex_workflow.remove_development_instances(
-    p_application_id => 102);
+    apex_workflow.remove_development_instances(
+        p_application_id => 101
+    );
+    
+    delete from requests;
+    
+    commit;
 end;
